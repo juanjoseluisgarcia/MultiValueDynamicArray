58688fcf3d3e129c8cbcc9528e2cd00095da1d3d
juanjoseluisgarcia <juanjoseluisgarcia@gmail.com>
2023-01-14 00:45:49 +0000

    first commit

---
+++ MultivalueDynamicArray/MultiValueDynamicArray.cs
@@ -0,0 +1,374 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace MultivalueDynamicArray
+{
+    /// <summary>
+    /// Defines a class for Multivalue Dynamic Array.
+    /// </summary>
+    public class MultiValueDynamicArray
+    {
+        /// <summary>
+        /// The unformatted matrix.
+        /// </summary>
+        private string _arrayString;
+
+        private const char Am = (char) 254;
+        private const char Vm = (char) 253;
+        private const char Sv = (char) 252;
+
+        #region private methods:
+
+        private string GetValue(int am)
+        {
+            var values = _arrayString.Split(Am);
+
+            if (values.Length < am)
+                return string.Empty;
+
+            return values[am - 1];
+        }
+
+        private string GetValue(int am, int vm, bool subValue = false)
+        {
+            var values = GetValue(am).Split(Vm);
+            
+            return values.Length < vm ? string.Empty : subValue ? values[vm - 1] : values[vm];
+        }
+
+        private string GetValue(int am, int vm, int sv)
+        {
+            var values = GetValue(am, vm, true).Split(Sv);
+
+            return values.Length < sv ? string.Empty : values[sv - 1];
+        }
+
+        /// <summary>
+        /// Sets the value of the matrix
+        /// </summary>
+        /// <param name="attribute">Attribute number</param>
+        /// <param name="value">value</param>
+        private void SetValue(int attribute, string value)
+        {
+            var values = _arrayString.Split(Am);
+            if (values.Length < attribute)
+            {
+                var str = new string[attribute];
+                values.CopyTo(str, 0);
+                for (var i = values.Length; i < attribute; i++)
+                {
+                    str[i] = string.Empty;
+                }
+
+                values = str;
+            }
+
+            values[attribute - 1] = value;
+            _arrayString = string.Join(Am.ToString(CultureInfo.InvariantCulture), values);
+        }
+
+        private void SetValue(int attribute, int vm, string value)
+        {
+            var values = _arrayString.Split(Am);
+            if (values.Length < attribute)
+            {
+                var str = new string[attribute];
+                values.CopyTo(str, 0);
+                for (var i = values.Length; i < attribute; i++)
+                {
+                    str[i] = string.Empty;
+                }
+
+                str.CopyTo(values, 0);
+            }
+
+            var vmValues = values[attribute - 1].IndexOf(Vm) == -1
+                ? Array.Empty<string>()
+                : values[attribute - 1].Split(Vm);
+
+            vmValues = SetVm(vmValues, vm, value);
+
+            values[attribute - 1] = JoinMultiValue(Vm, values[attribute - 1], vmValues[vm - 1]);
+            _arrayString = string.Join(Am.ToString(CultureInfo.InvariantCulture), values);
+        }
+
+        private void SetValue(int attribute, int vm, int sVm, string value)
+        {
+            var values = _arrayString.Split(Am);
+
+            if (values.Length < attribute)
+            {
+                var str = new string[attribute];
+                values.CopyTo(str, 0);
+                for (var i = values.Length; i < attribute; i++)
+                {
+                    str[i] = string.Empty;
+                }
+
+                values = str;
+            }
+
+            // var vmValues = values[attribute - 1].IndexOf(Vm) == -1
+            //     ? Array.Empty<string>()
+            //     : values[attribute - 1].Split(Vm);
+            var vmValues = values[attribute - 1].Split(Vm);
+            vmValues = SetVm(vmValues, vm);
+            
+            // var subValues =  vmValues[vm - 1].IndexOf(Sv) == -1
+            //     ? Array.Empty<string>()
+            //     : vmValues[vm - 1].Split(Sv);
+            var subValues = vmValues[vm - 1].Split(Sv);
+            subValues = SetVm(subValues, sVm, value);
+
+            vmValues[vm - 1] = string.Join(Sv.ToString(CultureInfo.CurrentCulture), subValues);
+
+            values[attribute - 1] = string.Join(Vm.ToString(CultureInfo.CurrentCulture), vmValues);
+            _arrayString = string.Join(Am.ToString(CultureInfo.InvariantCulture), values);
+            
+        }
+
+        private static string[] SetVm(string[] vmValues, int vm)
+        {
+            if (vmValues.Length < vm)
+            {
+                var str = new string[vm];
+                vmValues.CopyTo(str, 0);
+                for (var i = vmValues.Length; i < vm; i++)
+                {
+                    str[i] = string.Empty;
+                }
+
+                vmValues = str;
+            }
+
+            return vmValues;
+        }
+        
+        private static string[] SetSvm(string[] svmValues, int svm)
+        {
+            if (svmValues.Length < svm)
+            {
+                var str = new string[svm];
+                svmValues.CopyTo(str, 0);
+                for (var i = 0; i < svm; i++)
+                {
+                    str[i] = string.Empty;
+                }
+
+                svmValues = str;
+            }
+
+            return svmValues;
+        }
+
+        private static string[] SetVm(string[] vmValues, int vm, string value)
+        {
+            vmValues = SetVm(vmValues, vm);
+            vmValues[vm - 1] = value;
+            return vmValues;
+        }
+
+        private static string JoinMultiValue(char symbol, string destination, string mv)
+        {
+            var result = new StringBuilder();
+            result.Append(destination);
+            result.Append($"{symbol}{mv}");
+
+            return result.ToString();
+        }
+        
+        private static char GetPreviousChar(char symbol)
+        {
+            switch (symbol)
+            {
+                case Sv:
+                    return Vm;
+                case Vm:
+                    return Am;
+                default:
+                    return symbol;
+            }
+        }
+
+        #endregion
+
+        #region public constructors
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="MultiValueDynamicArray"/> class.
+        /// </summary>
+        public MultiValueDynamicArray()
+        {
+            _arrayString = string.Empty;
+        }
+
+        /// <summary>
+        /// /// Initializes a new instance of the <see cref="MultiValueDynamicArray"/> class.
+        /// </summary>
+        /// <param name="value"></param>
+        public MultiValueDynamicArray(string value) : this()
+        {
+            _arrayString = ConvertFromD3Array(value);
+        }
+
+        #endregion
+
+        #region overriden methods:
+
+        /// <summary>
+        /// Returns the string that is inside the matrix with all the values and subvalues chars set.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return ConvertToD3Array(_arrayString);
+        }
+
+        private static string ToString(MultiValueDynamicArray matrix)
+        {
+            return matrix.ToString();
+        }
+
+        #endregion
+
+        #region Helper Methods:
+
+        private static string ConvertToD3Array(string array)
+        {
+            array = array.Replace((char) Am, '^');
+            array = array.Replace((char) Vm, ']');
+            array = array.Replace((char) Sv, '\\');
+            return array;
+        }
+
+        private static string ConvertFromD3Array(string array)
+        {
+            array = array.Replace('^', Am);
+            array = array.Replace(']', Vm);
+            array = array.Replace('\\', Sv);
+
+            return array;
+        }
+
+        #endregion
+
+        #region overloaded operators:
+
+        /// <summary>
+        /// Implicit operator for string assignments.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static implicit operator string(MultiValueDynamicArray value)
+        {
+            return ToString(value);
+        }
+
+        /// <summary>
+        /// Implicit operator for D3Matrix assignments.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static implicit operator MultiValueDynamicArray(string value)
+        {
+            return new MultiValueDynamicArray(value);
+        }
+
+        /// <summary>
+        /// Implicit operator for integer assignments.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static implicit operator MultiValueDynamicArray(int value)
+        {
+            var result = new MultiValueDynamicArray
+            {
+                [1] = value.ToString()
+            };
+            return result;
+        }
+
+        /// <summary>
+        /// Implicit operator for double assignments.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static implicit operator MultiValueDynamicArray(double value)
+        {
+            var result = new MultiValueDynamicArray
+            {
+                [1] = value.ToString(CultureInfo.CurrentCulture)
+            };
+            return result;
+        }
+
+        /// <summary>
+        /// Implicit operator for decimal assignments.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static implicit operator MultiValueDynamicArray(decimal value)
+        {
+            var result = new MultiValueDynamicArray
+            {
+                [1] = value.ToString(CultureInfo.InvariantCulture)
+            };
+            return result;
+        }
+
+        /// <summary>
+        /// Implicit operator for float assignments.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static implicit operator MultiValueDynamicArray(float value)
+        {
+            var result = new MultiValueDynamicArray
+            {
+                [1] = value.ToString(CultureInfo.InvariantCulture)
+            };
+            return result;
+        }
+
+        #endregion
+
+        #region public properties:
+
+        /// <summary>
+        /// Gets or sets the value of the attribute of the matrix at the current index
+        /// </summary>
+        /// <param name="attribute">attribute's number</param>
+        public string this[int attribute]
+        {
+            get => GetValue(attribute);
+            set => SetValue(attribute, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the value of the attribute of the matrix at the current index
+        /// </summary>
+        /// <param name="attribute">attribute</param>
+        /// <param name="mvValue">value</param>
+        public string this[int attribute, int mvValue]
+        {
+            get => GetValue(attribute, mvValue);
+            set => SetValue(attribute, mvValue, value);
+        }
+
+
+        /// <summary>
+        /// Gets the number of attributes in the matrix.    
+        /// </summary>
+        /// <param name="attribute">attribute</param>
+        /// <param name="mvValue">value</param>
+        /// <param name="subValue">subvalue</param>
+        public string this[int attribute, int mvValue, int subValue]
+        {
+            get => _arrayString.Split(Am)[attribute - 1].Split(Vm)[mvValue - 1].Split(Sv)[subValue - 1];
+            set => SetValue(attribute, mvValue, subValue, value);
+        }
+
+        #endregion
+    }
+}
---
+++ MultivalueDynamicArrayTests/MultiValueArrayTests.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using MultivalueDynamicArray;
+using NUnit.Framework;
+
+namespace MultivalueDynamicArrayTests
+{
+    [TestFixture]
+    public class MultiValueArrayTests
+    {
+        private string _d3Data = "AAAA\\BBBB\\CCCC]BBBB]CCCC^BBBB";
+        private MultiValueDynamicArray _dynamicArray;
+
+        [SetUp]
+        public void Setup()
+        {
+            _dynamicArray = new MultiValueDynamicArray();
+        }
+
+        [Test]
+        public void should_return_d3Matrix_from_a_string()
+        {
+            _dynamicArray = _d3Data;
+            Assert.AreEqual(_dynamicArray[1, 1, 1], "AAAA");
+            Assert.AreEqual(_dynamicArray[1, 1, 2], "BBBB");
+            Assert.AreEqual(_dynamicArray[1, 1, 3], "CCCC");
+        }
+
+        [Test]
+        public void should_return_correctly_formatted_string()
+        {
+            _dynamicArray = _d3Data;
+            string test = _dynamicArray;
+            Assert.AreEqual(_dynamicArray.ToString(), _d3Data);
+            Assert.AreEqual(test, _d3Data);
+        }
+
+        [Test]
+        public void should_handle_attributes_correctly()
+        {
+            _dynamicArray = _d3Data;
+            _dynamicArray[4] = "Test";
+            Assert.AreEqual(_dynamicArray[4], "Test");
+        }
+
+        [Test]
+        public void should_handle_values_correctly()
+        {
+            _dynamicArray = _d3Data;
+            _dynamicArray[4] = "Test";
+            _dynamicArray[4, 1] = "Test2";
+            _dynamicArray[4, 2] = "Test3";
+
+            Assert.AreEqual(_dynamicArray[4, 1], "Test2");
+            Assert.AreEqual(_dynamicArray[4, 2], "Test3");
+        }
+
+        [Test]
+        public void should_handle_subvalues_correctly()
+        {
+            _dynamicArray = _d3Data;
+            _dynamicArray[4] = "Test";
+            _dynamicArray[4, 1, 1] = "Test2";
+            _dynamicArray[4, 2, 2] = "Test3";
+
+            Assert.AreEqual(_dynamicArray[4, 1, 1], "Test2");
+            Assert.AreEqual(_dynamicArray[4, 2, 2], "Test3");
+        }
+
+        [Test]
+        public void should_create_a_new_matrix_correctly()
+        {
+            var matrix = new MultiValueDynamicArray
+            {
+                [1] = "Test",
+                [1, 1] = "Test2",
+                [1, 2] = "Test32",
+                [1, 2, 1] = "Test4",
+                [1, 2, 2] = "Test5",
+            };
+
+            for (var i = 1; i <= 5; i++)
+            {
+                matrix[2, 1, i] = "Test" + i;
+                matrix[2, 2, i] = "another test" + i;
+                matrix[2, 3, i] = "subtest " + i;
+            }
+            
+            
+            Assert.AreEqual(matrix[1, 2, 1], "Test4");
+            Assert.AreEqual(matrix[1, 2, 2], "Test5");
+            Assert.AreEqual(matrix[2, 1, 1], "Test1");
+            Assert.AreEqual(matrix[2, 1, 2], "Test2");
+            Assert.AreEqual(matrix[2, 1, 3], "Test3");
+            Assert.AreEqual(matrix[2, 1, 4], "Test4");
+            Assert.AreEqual(matrix[2, 1, 5], "Test5");
+            Assert.AreEqual(matrix[2, 2, 1], "another test1");
+            Assert.AreEqual(matrix[2, 2, 2], "another test2");
+            Assert.AreEqual(matrix[2, 2, 3], "another test3");
+            Assert.AreEqual(matrix[2, 2, 4], "another test4");
+            Assert.AreEqual(matrix[2, 2, 5], "another test5");
+            Assert.AreEqual(matrix[2, 3, 1], "subtest 1");
+            Assert.AreEqual(matrix[2, 3, 2], "subtest 2");
+            Assert.AreEqual(matrix[2, 3, 3], "subtest 3");
+            Assert.AreEqual(matrix[2, 3, 4], "subtest 4");
+            Assert.AreEqual(matrix[2, 3, 5], "subtest 5");
+        }
+
+        [Test]
+        public void should_be_able_to_resize()
+        {
+            
+            var matrix = new MultiValueDynamicArray
+            {
+                [1] = "Test",
+                [2] = "field 2",
+                [3] = "field 3",
+            };
+
+            for (var i = 1; i <= 5; i++)
+            {
+                matrix[9, 1, i] = "Test" + i;
+                matrix[9, 2, i] = "another test" + i;
+                matrix[9, 3, i] = "subtest " + i;
+            }
+            
+            Assert.That(matrix[9, 1, 1], Is.EqualTo("Test1"));
+            Assert.That(matrix[9, 1, 2], Is.EqualTo("Test2"));
+            Assert.That(matrix[9, 1, 3], Is.EqualTo("Test3"));
+            Assert.That(matrix[9, 1, 4], Is.EqualTo("Test4"));
+            Assert.That(matrix[9, 1, 5], Is.EqualTo("Test5"));
+            Assert.That(matrix[9, 2, 1], Is.EqualTo("another test1"));
+            Assert.That(matrix[9, 2, 2], Is.EqualTo("another test2"));
+            Assert.That(matrix[9, 2, 3], Is.EqualTo("another test3"));
+            Assert.That(matrix[9, 2, 4], Is.EqualTo("another test4"));
+            Assert.That(matrix[9, 2, 5], Is.EqualTo("another test5"));
+            Assert.That(matrix[9, 3, 1], Is.EqualTo("subtest 1"));
+            Assert.That(matrix[9, 3, 2], Is.EqualTo("subtest 2"));
+            Assert.That(matrix[9, 3, 3], Is.EqualTo("subtest 3"));
+            Assert.That(matrix[9, 3, 4], Is.EqualTo("subtest 4"));
+            Assert.That(matrix[9, 3, 5], Is.EqualTo("subtest 5"));
+        }
+    }
+}
